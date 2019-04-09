8c383bd5b2bab3eb3c5fddeffd6ef9e100488250
stefanusz <Stefanus.Husin@treasury.qld.gov.au>
2019-04-09 15:29:51 +1000

    Add entry point for nodes, check instantiation for node express, add placeholder for search entry point

---
+++ Services/router/routes/FlexibleDebt/search.js
@@ -0,0 +1,15 @@
+/*eslint no-console: 0, no-unused-vars: 0, no-shadow: 0, new-cap: 0*/
+"use strict";
+const express = require("express");
+const async = require("async");
+
+module.exports = function() {
+	let app = express.Router();
+
+	//root entry path for placeholder
+	app.get("/", function(req, res) {
+		res.send("Instantiation of Services search");
+	});
+
+	return app;
+};
